382b50f9ad380b28220641596178666e7f623d54
engravesunny <3170557037@qq.com>
2023-01-09 21:06:46 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,3 @@
     <router-view></router-view>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'blog-app',
-  data() {
-    return {
-
-    };
-  },
-
-  mounted() {
-
-  },
-
-  methods: {
-
-  },
-};
-</script>
-
-<style lang="scss" scoped>
-
-</style>
